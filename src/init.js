26a6d6c348a44a9feae81d129d17ddf5c4ed431d
s1nyx <sinyx.contact@gmail.com>
2023-12-04 11:55:32 +0100

    feat: added userRoutes to the express app

--- src/init.js
+++ src/init.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const userRoutes = require('./routes/userRoutes')
 const app = express()
 
 const PORT = 3000
+
+app.use('/', userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
