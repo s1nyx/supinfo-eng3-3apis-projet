938e1d3c0a3f64f82349c810eea1d6c10fcb7f0a
s1nyx <sinyx.contact@gmail.com>
2023-12-04 14:52:53 +0100

    tweak: prepared user controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,44 @@
+const User = require('../models/user')
+
 exports.createUser = async (request, response) => {
+    const { email, username, password, role } = request.body
+    const user = new User.create({ email, username, password, role })
+
+    if (!user) {
+        return response.status(400).json({ error: "User not created" })
+    }
+
+    return response.status(201).json({ user })
 }
 
 exports.getUser = async (request, response) => {
+    // TODO: valider le param id
+
+    const user = await User.findById(request.params.id)
+
+    if (!user) {
+        return response.status(404).json({ error: "User not found" })
+    }
+
+    return response.status(200).json({ user })
 }
 
 exports.updateUser = async (request, response) => {
+    const user = await User.findByIdAndUpdate(request.params.id, request.body)
+
+    if (!user) {
+        return response.status(404).json({ error: "User not found" })
+    }
+
+    return response.status(200).json({ user })
 }
 
 exports.deleteUser = async (request, response) => {
+    const user = await User.findByIdAndDelete(request.params.id)
+
+    if (!user) {
+        return response.status(404).json({ error: "User not found" })
+    }
+
+    return response.status(200).json({ message: "User deleted successfully" })
 }
--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,6 @@ const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, required: true },
-    iv: { type: String, required: true }
 })
 
 module.exports = mongoose.model('User', userSchema)
