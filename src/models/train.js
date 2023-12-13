86c6c40143a5232b55e8b690b9ca327ebf057323
Coding Glitch <47162074+codingglitch@users.noreply.github.com>
2023-12-13 05:13:42 -0300

    tweak: update train & trainStation exports

--- src/models/train.js
+++ src/models/train.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
-exports.trainSchema = mongoose.Schema({
+const trainSchema = mongoose.Schema({
     name: {type: String, required: true},
     start_station: {type: String, required: true},
     end_station: {type: String, required: true},
     time_of_Departure: {type: Date, required: true}
 })
+
+module.exports = mongoose.model("Train", trainSchema, "trains")
--- src/models/trainStation.js
+++ src/models/trainStation.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
-exports.stationSchema = mongoose.Schema({
+const stationSchema = mongoose.Schema({
     name: {type: String, required: true},
     open_hour: {type: Date, required: true},
     close_hour: {type: Date, required: true},
     image: {type: String, required: true}
 })
+
+module.exports = mongoose.model("TrainStation", stationSchema, "stations")
