c992fd6edf63258f5099f0d16f052a85427fe40c
s1nyx <sinyx.contact@gmail.com>
2023-12-14 15:29:02 +0100

    tweak: added connection to the db

--- src/init.js
+++ src/init.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const mangoose = require('mongoose')
+
 const userRoutes = require('./routes/userRoutes')
 const authRoutes = require('./routes/authRoutes')
 const trainRoutes = require(('./routes/trainRoutes'))
 const app = express()
 
 const PORT = 3000
+
+mangoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.0', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to database'))
+    .catch((err) => console.log(err))
+
+app.use(express.json())
 
 app.use('/users', userRoutes)
 app.use('/trains', trainRoutes)
