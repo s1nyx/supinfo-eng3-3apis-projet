d29382e4b0120aad0da416864efc85442a21691a
s1nyx <sinyx.contact@gmail.com>
2023-12-14 17:28:26 +0100

    feat: disabled multiple login when already logged

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -6,4 +6,12 @@ const isLoggedIn = (request, response, next) => {
     response.status(401).json({ error: "Unauthorized" })
 }
 
-module.exports = isLoggedIn
+const notAuthenticated = (request, response, next) => {
+    if (!request.isAuthenticated()) {
+        return next()
+    }
+
+    response.status(400).json({ error: "Already authenticated" })
+}
+
+module.exports = { isLoggedIn, notAuthenticated }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 const authController = require('../controllers/authController')
 const passport = require('passport')
-const isLoggedIn = require('../middlewares/authMiddleware')
+const { isLoggedIn, notAuthenticated } = require('../middlewares/authMiddleware')
 
 const router = Router()
 
 router.post('/signup', authController.signup)
-router.post('/signin', passport.authenticate('local'), authController.signin)
+router.post('/signin', notAuthenticated, passport.authenticate('local'), authController.signin)
 router.post('/signout', isLoggedIn, authController.signout)
 
 module.exports = router
