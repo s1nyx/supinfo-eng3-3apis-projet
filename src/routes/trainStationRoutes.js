f51ca502118c5f1bbd2269cb122d1176b28ab24f
s1nyx <sinyx.contact@gmail.com>
2023-12-17 17:16:41 +0100

    feat: added role admin check on POST, PATCH and DELETE for /stations

--- src/routes/trainStationRoutes.js
+++ src/routes/trainStationRoutes.js
@@ -1,13 +1,14 @@
 const { Router } = require('express')
 const trainStationController = require('../controllers/trainStationController')
-const authMiddleware = require('../middlewares/authMiddleware')
+const { isLoggedIn } = require('../middlewares/authMiddleware')
+const { authorizeRoles } = require('../middlewares/roleMiddleware')
 const multer = require('multer')
 
 const upload = multer({dest: 'uploads/'})
 const router = Router()
 
 // Création d'un train
-router.post("/", trainStationController.createTrainStation)
+router.post("/", isLoggedIn, authorizeRoles(['admin']), trainStationController.createTrainStation)
 
 // Récupérer les informations de tout les trains
 router.get('/', trainStationController.getTrainStationList)
@@ -18,10 +19,10 @@ router.get('/:name', trainStationController.getTrainStation)
 router.put('/:name', upload.single("image"), trainStationController.uploadStationImage)
 
 // Mise à jour des informations d'une station
-router.patch('/:id', trainStationController.updateTrainStation)
+router.patch('/:id', isLoggedIn, authorizeRoles(['admin']), trainStationController.updateTrainStation)
 
 // Suppressions des informations d'une station
 // Utilise l'ID comme forme de confirmation (connaître le nom n'est pas suffisant)
-router.delete('/:id', trainStationController.deleteTrainStation)
+router.delete('/:id', isLoggedIn, authorizeRoles(['admin']), trainStationController.deleteTrainStation)
 
 module.exports = router
