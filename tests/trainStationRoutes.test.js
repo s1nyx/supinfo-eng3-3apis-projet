8e02e74fc61b98b9c0321c2c66811b7df4f49018
Coding Glitch <47162074+codingglitch@users.noreply.github.com>
2023-12-17 15:58:13 -0300

    wip: tests for train stations

---
+++ tests/trainStationRoutes.test.js
@@ -0,0 +1,88 @@
+const app = require('../src/init.js')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+let agent = chai.request.agent(app)
+const TrainStation = require('../src/models/trainStation')
+const User = require('../src/models/user')
+
+const expect = chai.expect
+
+let firstStationId
+let secondStationId
+
+const adminUser = {
+    email: 'admin.s@example.com',
+    username: 'adminS',
+    password: 'password123',
+    role: 'admin'
+}
+
+
+//1990-01-01 is only there to comply with the date format
+//Javascript date objects make it easier to handle validation
+const firstStation = {
+    name: "Bruxelles",
+    open_hour: "1990-01-01T08:00:00Z",
+    close_hour: "1990-01-01T20:00:00Z"
+}
+
+const secondStation = {
+    name: "Jurbise",
+    open_hour: "1990-01-01T08:00:00Z",
+    close_hour: "1990-01-01T22:00:00Z"
+}
+
+
+before(async function() {
+    // Clean the database
+    await TrainStation.deleteMany({})
+    await User.deleteMany({})
+
+    // Create and login as an admin
+    await new Promise((resolve, reject) => {
+        agent
+            .post('/users')
+            .send(adminUser)
+            .end((err, res) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(res)
+                }
+            });
+    });
+
+    await new Promise((resolve, reject) => {
+        agent
+            .post('/auth/signin')
+            .send({
+                email: adminUser.email,
+                password: adminUser.password
+            })
+            .end((err, res) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    console.log(res.status)
+                    resolve(res)
+                }
+            })
+    })
+})
+
+describe("POST /stations/", () => {
+    it("Should create stationA", (done) => {
+
+        agent
+            .post('/stations')
+            .send(firstStation)
+            .end( (err, res) => {
+                console.log(res.body)
+                expect(res).to.have.status(201)
+                expect(res.body).to.be.a('object')
+                done()
+            })
+    })
+
+})
