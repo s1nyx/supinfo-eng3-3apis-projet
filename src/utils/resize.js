342bd116cabac519200cd99080864addff4f028c
Coding Glitch <47162074+codingglitch@users.noreply.github.com>
2023-12-17 12:11:46 -0300

    feat: Resize large images

--- src/utils/resize.js
+++ src/utils/resize.js
@@ -1,7 +1,8 @@
 const fs = require("fs")
-//const sharp = require("sharp")
+const sharp = require("sharp")
 
 //Resize image to 200 x 200
-module.exports.resizeImage = (imagePath, format) => {
-    
+module.exports.resizeImage = (imagePath) => {
+    const data = fs.readFileSync(imagePath)
+    sharp(data).resize(200, 200).toFile(imagePath)
 }
