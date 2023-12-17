bf490b13b0f56e2834d6418268b5f9e13f3871d0
Coding Glitch <47162074+codingglitch@users.noreply.github.com>
2023-12-17 14:43:42 -0300

    feat: validate roles and input on ticket endpoints

--- src/routes/ticketRoutes.js
+++ src/routes/ticketRoutes.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const ticketController = require("../controllers/ticketController")
+const validateRequest = require("../middlewares/validateRequestMiddleware")
+const { ticketSchema, ticketIdSchema} = require("../validators/ticketValidator")
+const { isLoggedIn } = require("../middlewares/authMiddleware")
+const { authorizeRoles } = require("../middlewares/roleMiddleware")
 
 const router = express.Router()
 
 
-router.post('/', ticketController.bookTicket)
+router.post('/', validateRequest(ticketSchema), isLoggedIn, ticketController.bookTicket)
 
-router.get("/", ticketController.getTicketList)
+router.get("/", isLoggedIn, authorizeRoles(["admin"]),ticketController.getTicketList)
 
-router.put("/:id", ticketController.validateTicket)
+router.put("/:id", validateRequest(ticketIdSchema), isLoggedIn, authorizeRoles(["admin"]), ticketController.validateTicket)
 
 module.exports = router
